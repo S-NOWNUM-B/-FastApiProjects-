63da615c6b1fa67fa80f2fc443d8a5d4d7de2509
S-NOWNUM-B <mamayev.stas@gmail.com>
2025-05-29 23:21:04 +0500

    Update Files

--- FastApiProject_1/main.py
+++ FastApiProject_1/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Field, create_engine, select
+from sqlmodel import SQLModel, Field, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
-from typing import List
 from datetime import datetime
 from pydantic import BaseModel
 
